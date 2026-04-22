da2044066e859cf045205fb8c265cbe1133acadd
Dawid <dawid.wachecki@onet.pl>
2026-04-22 14:36:03 +0200

    feature: jakies podstawowe funkcje

--- algorithms.py
+++ algorithms.py
@@ -1,7 +1,29 @@
 #ZGADZA SIE, PODJEBAŁEM KOD SAMEMU SOBIE Z ZADANIA 1.
+from cmath import cos
+
 def horner(x, tablica_wspolczynnikow, dlugosc_tablicy):
     #Czyli zamiast np. y = 4*x*x*x + 3*x + 5 podaje się funkcje wielomian z argumentami: x, [4,0,3,5], 4
     y = tablica_wspolczynnikow[0]
     for i in range(1,dlugosc_tablicy,1):
         y = tablica_wspolczynnikow[i] + x * y
+    return y
+
+def funkcja_liniowa(x):
+    #y = 2x - 5
+    y = 2*x-5
+    return y
+
+def funkcja_abs(x):
+    #y = |0.5x + 3|
+    y = abs(0.5*x + 3)
+    return y
+
+def funkcja_wielomian(x):
+    # y = 2x^3 - 5x^2 + 2x
+    y = horner(x, [2, -5, 2, 0], 4)
+    return 0
+
+def funkcja_trygonometryczna(x):
+    # y = cos(x)
+    y = cos(x)
     return y
--- main.py
+++ main.py
@@ -2,13 +2,50 @@
 # Kacper Skoczylas 254864
 #Zadanie 3: wariant 1 - Interpolacja Lagrange'a dla węzłów równoodległych
 
+import algorithms as alg
+
+#wybór funkcji jako słownik, gdzie kluczem jest numer funkcji, a wartością jest sama funkcja.
+def wybranaFunkcja(wybor):
+    funkcje = {
+        1: alg.funkcja_liniowa,
+        2: alg.funkcja_abs,
+        3: alg.funkcja_wielomian,
+        4: alg.funkcja_trygonometryczna,
+    }
+    return funkcje.get(wybor)
+
+def nazwaFunkcji(wybor):
+    nazwa = {
+        1: "y = 2x - 5",
+        2: "y = |0.5x + 3|",
+        3: "y = 2x^3 - 5x^2 + 2x",
+        4: "y = cos(x)",
+    }
+    return nazwa.get(wybor)
 def main():
     print("================================================")
     print("ZADANIE 3.")
     print("================================================\n")
-    print("WYBIERZ FUNKCJĘ NIELINIOWĄ:")
-    print("PODAJ PRZEDZIAŁ [a,b]:")
-    print("PODAJ LICZBĘ WĘZŁÓW:")
+    print("WYBIERZ FUNKCJĘ")
+    #wypisujemy opcje w menu
+    for i in range(1, 5, 1):
+        print(f"{i}. {nazwaFunkcji(i)}")
+    #pobieramy wybór od użytkownika
+    wyborFunkcji = int(input("\nWYBRANA FUNKCJA: "))
+    if (wyborFunkcji < 1 or wyborFunkcji > 7):
+        print("Błąd: wpisano niepoprawny numer.")
+        return
+
+    print("================================================\n")
+    print("PODAJ PRZEDZIAŁ [a,b]")
+    a = float(input("PODAJ WARTOŚĆ 'a': "))
+    b = float(input("PODAJ WARTOŚĆ 'b': "))
+    print(f"WYBRANY PRZEDZIAŁ: [{a}, {b}]")
+    print("================================================\n")
+
+    n = int(input("PODAJ LICZBĘ WĘZŁÓW:"))
+    print(f"WYBRANA LICZBA WĘZŁÓW: {n}")
+
 
 if __name__ == "__main__":
     main()
